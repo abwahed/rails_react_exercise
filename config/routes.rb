095a4d2a7a2e549a5b4cde8d37045b78b51baa65
Abdul Wahed <abwahed23@gmail.com>
2024-04-28 02:43:50 +0600

    initial commit 2

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'exercises/index'
+      post 'exercises/create'
+      get '/show/:id', to: 'exercises#show'
+      delete '/destroy/:id', to: 'exercises#destroy'
+    end
+  end
+  # get 'homepage/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  root "homepage#index"
+  get '/*path' => 'homepage#index'
+end
